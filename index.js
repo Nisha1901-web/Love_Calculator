cdb79ec74e30fed8a239bb94d45a7dbde8ad9c8c
Nisha Deshmukh <122263294+Nisha1901-web@users.noreply.github.com>
2023-04-01 09:24:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
-var loveScore = Math.random() * 100;
+let loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
-var loveScoreFinal = " Your Love Score is " + "<i style='color:green'>" +loveScore + "%" + "</i>";
+let loveScoreFinal = " Your Love Score is " + "<i style='color:green'>" +loveScore + "%" + "</i>";
 
     
     function getName(){
 
-        var name1 = document.getElementById("person1").value;
+        let name1 = document.getElementById("person1").value;
         name1 =name1.toLowerCase();
-        var name2 = document.getElementById("person2").value;
+        let name2 = document.getElementById("person2").value;
         name2 = name2.toLowerCase();
 
 
